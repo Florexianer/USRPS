bef3600b32614ecbf67e2ee94b2800176704f8ed
Florexianer <8111@htl.rennweg.at>
2022-04-28 11:54:10 +0200

    US 4

--- data/console.sql
+++ data/console.sql
@@ -1,3 +1,4 @@
+DROP DATABASE USRPS;
 CREATE DATABASE USRPS CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE USRPS;
 
@@ -19,3 +20,18 @@ CREATE TABLE Round (
     FOREIGN KEY (fk_pk_player0) REFERENCES Player(pk_ID),
     FOREIGN KEY (fk_pk_player1) REFERENCES Player(pk_ID)
 );
+
+
+INSERT INTO Player(Vorname, Nachname)
+VALUES ('German','Ömer'),
+       ('Rodriquez','Hernando'),
+       ('Fort','Nite'),
+       ('Joe','Mam'),
+       ('Paul','Paul');
+
+INSERT INTO Round(fk_pk_player0, fk_pk_player1, pick0, pick1, datetime, winner)
+VALUES (1,2,'r','p','2022/04/28 11:19:01',1),
+       (2,3,'s','p','2022/02/12 12:18:01',0),
+       (3,4,'p','r','2021/01/06 08:12:22',0),
+       (2,5,'s','s','2018/11/05 22:10:06',null),
+       (4,5,'r','s','2020/04/06 01:16:52',0);
